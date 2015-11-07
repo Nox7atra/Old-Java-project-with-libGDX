9cb08249f0414e63258bd1f8700e3842382f853a
Nox7atra <warsong94@gmail.com>
2015-11-07 10:53:08 +0300

    Refactor a class , because the code is written in the first year of university was too disgusting.

--- screen/LevelsScreen.java
+++ screen/LevelsScreen.java
@@ -24,23 +24,14 @@ public class LevelsScreen implements Screen, InputProcessor{
 	private Table table = new Table();
 	private Skin skin = new Skin(Gdx.files.internal("data/menuSkin.json"), 
 			new TextureAtlas(Gdx.files.internal("data/menuAtlas.atlas")));
-	private TextButton buttonLvl12,
-	        buttonLvl1,
-	        buttonLvl2,
-	        buttonLvl3,
-    		buttonLvl4,
-	        buttonLvl5,
-	        buttonLvl6,
-    		buttonLvl7,
-	        buttonLvl8,
-    		buttonLvl9,
-	        buttonLvl10,
-	        buttonLvl11;
+	private Array<TextButton> buttons;
 	
 	private Label title = new Label("Levels",skin);
 	
 	public static float CAMERA_WIDTH = 1280f;
 	public static  float CAMERA_HEIGHT = 640f;
+
+	private int buttonsInRow = 4;
 	int BUTTONLVL_WIDTH = 140;
 	int BUTTONLVL_HEIGHT = 140;
 	float ppuX, ppuY;
@@ -59,6 +50,7 @@ public class LevelsScreen implements Screen, InputProcessor{
 		ppuX = Gdx.graphics.getWidth() / CAMERA_WIDTH;
 		ppuY = Gdx.graphics.getHeight() / CAMERA_HEIGHT;
 		
+		buttons = new Array<TextButton>();
 		backgroundSprite = new Sprite(backgroundTexture);
 		backgroundSprite.setSize(Gdx.graphics.getWidth() , Gdx.graphics.getHeight());
 		
@@ -72,118 +64,25 @@ public class LevelsScreen implements Screen, InputProcessor{
 		backgroundSprite.draw(spriteBatch);
 	}
 	private void buttonInit(){
-		buttonLvl1 = new TextButton("1", skin);
-		buttonLvl2 = new TextButton("2", skin);
-        buttonLvl3 = new TextButton("3", skin);
-        buttonLvl4 = new TextButton("4", skin);
-		buttonLvl5 = new TextButton("5", skin);
-        buttonLvl6 = new TextButton("6", skin);
-        buttonLvl7 = new TextButton("7", skin);
-		buttonLvl8 = new TextButton("8", skin);
-        buttonLvl9 = new TextButton("9", skin);
-		buttonLvl10 = new TextButton("10", skin);
-        buttonLvl11 = new TextButton("11", skin);
-        buttonLvl12 = new TextButton("12", skin);
-        buttonLvl1.getLabel().setFontScale((float) (ppuX), ppuY);
-        buttonLvl2.getLabel().setFontScale((float) (ppuX), ppuY);
-        buttonLvl3.getLabel().setFontScale((float) (ppuX), ppuY);
-        buttonLvl4.getLabel().setFontScale((float) (ppuX), ppuY);
-        buttonLvl5.getLabel().setFontScale((float) (ppuX), ppuY);
-        buttonLvl6.getLabel().setFontScale((float) (ppuX), ppuY);
-        buttonLvl7.getLabel().setFontScale((float) (ppuX), ppuY);
-        buttonLvl8.getLabel().setFontScale((float) (ppuX), ppuY);
-        buttonLvl9.getLabel().setFontScale((float) (ppuX), ppuY);
-        buttonLvl10.getLabel().setFontScale((float) (ppuX), ppuY);
-        buttonLvl11.getLabel().setFontScale((float) (ppuX), ppuY);
-        buttonLvl12.getLabel().setFontScale((float) (ppuX), ppuY);
+		for(int i = 0; i < buttons.length(); i++){
+			buttons[i] = new TextButton(String.valueOf(i + 1), skin);
+			buttons[i].getLabel().setFontScale((float) (ppuX), ppuY);
+		}
 	}
 	void onClick(){
-		buttonLvl1.addListener(new ClickListener(){
-	        @Override
-	        public void clicked(InputEvent event, float x, float y) {
-	            //Same way we moved here from the Splash Screen
-	            //We set it to new Splash because we got no other screens
-	            //otherwise you put the screen there where you want to go
-	            ((MyGame)Gdx.app.getApplicationListener()).setScreen(new LoadingScreen(game, 1));
-	        }
-	    });
-		buttonLvl2.addListener(new ClickListener(){
-	        @Override
-	        public void clicked(InputEvent event, float x, float y) {
-	        	((MyGame)Gdx.app.getApplicationListener()).setScreen(new LoadingScreen(game, 2));
-	            // or System.exit(0);
-	        }
-	    });
-		buttonLvl3.addListener(new ClickListener(){
-	        @Override
-	        public void clicked(InputEvent event, float x, float y) {
-	        	((MyGame)Gdx.app.getApplicationListener()).setScreen(new LoadingScreen(game, 3));
-	            // or System.exit(0);
-	        }
-	    });
-		buttonLvl4.addListener(new ClickListener(){
-	        @Override
-	        public void clicked(InputEvent event, float x, float y) {
-	        	((MyGame)Gdx.app.getApplicationListener()).setScreen(new LoadingScreen(game, 4));
-	            // or System.exit(0);
-	        }
-	    });
-		buttonLvl5.addListener(new ClickListener(){
-	        @Override
-	        public void clicked(InputEvent event, float x, float y) {
-	        	((MyGame)Gdx.app.getApplicationListener()).setScreen(new LoadingScreen(game, 5));
-	            // or System.exit(0);
-	        }
-	    });
-		buttonLvl6.addListener(new ClickListener(){
-	        @Override
-	        public void clicked(InputEvent event, float x, float y) {
-	        	((MyGame)Gdx.app.getApplicationListener()).setScreen(new LoadingScreen(game, 6));
-	            // or System.exit(0);
-	        }
-	    });
-		buttonLvl7.addListener(new ClickListener(){
-	        @Override
-	        public void clicked(InputEvent event, float x, float y) {
-	        	((MyGame)Gdx.app.getApplicationListener()).setScreen(new LoadingScreen(game, 7));
-	            // or System.exit(0);
-	        }
-	    });
-		buttonLvl8.addListener(new ClickListener(){
-	        @Override
-	        public void clicked(InputEvent event, float x, float y) {
-	        	((MyGame)Gdx.app.getApplicationListener()).setScreen(new LoadingScreen(game, 8));
-	            // or System.exit(0);
-	        }
-	    });
-		buttonLvl9.addListener(new ClickListener(){
-	        @Override
-	        public void clicked(InputEvent event, float x, float y) {
-	        	((MyGame)Gdx.app.getApplicationListener()).setScreen(new LoadingScreen(game, 9));
-	            // or System.exit(0);
-	        }
-	    });
-		buttonLvl10.addListener(new ClickListener(){
-	        @Override
-	        public void clicked(InputEvent event, float x, float y) {
-	        	((MyGame)Gdx.app.getApplicationListener()).setScreen(new LoadingScreen(game, 10));
-	            // or System.exit(0);
-	        }
-	    });
-		buttonLvl11.addListener(new ClickListener(){
-	        @Override
-	        public void clicked(InputEvent event, float x, float y) {
-	        	((MyGame)Gdx.app.getApplicationListener()).setScreen(new LoadingScreen(game, 11));
-	            // or System.exit(0);
-	        }
-	    });
-		buttonLvl12.addListener(new ClickListener(){
-	        @Override
-	        public void clicked(InputEvent event, float x, float y) {
-	        	((MyGame)Gdx.app.getApplicationListener()).setScreen(new LoadingScreen(game, 12));
-	            // or System.exit(0);
-	        }
-	    });
+		for(int i = 0; i < buttons.length(); i++){
+			button[i].addListener(new ClickListener(){
+		        @Override
+		        public void clicked(InputEvent event, float x, float y) {
+		            //Same way we moved here from the Splash Screen
+		            //We set it to new Splash because we got no other screens
+		            //otherwise you put the screen there where you want to go
+		            ((MyGame)Gdx.app.getApplicationListener()).setScreen(new LoadingScreen(game, i + 1));
+		        }
+		    });
+		}
+		
+		
 	}
 	@Override
 	public void render(float delta) {
@@ -202,6 +101,8 @@ public class LevelsScreen implements Screen, InputProcessor{
 		  
 	}
 
+
+
 	@Override
 	public void show() {
 		
@@ -209,22 +110,12 @@ public class LevelsScreen implements Screen, InputProcessor{
 		onClick();
 		table.row();
 	    table.add(title).padBottom(20).colspan(4);
-	    table.row();
-		
-	    table.add(buttonLvl1).size(ppuX * BUTTONLVL_WIDTH,ppuY * BUTTONLVL_HEIGHT).padBottom(20).padLeft(10).padRight(10);
-	    table.add(buttonLvl2).size(ppuX * BUTTONLVL_WIDTH,ppuY * BUTTONLVL_HEIGHT).padBottom(20).padLeft(10).padRight(10);
-	    table.add(buttonLvl3).size(ppuX * BUTTONLVL_WIDTH,ppuY * BUTTONLVL_HEIGHT).padBottom(20).padLeft(10).padRight(10);
-	    table.add(buttonLvl4).size(ppuX * BUTTONLVL_WIDTH,ppuY * BUTTONLVL_HEIGHT).padBottom(20).padLeft(10).padRight(10);
-	    table.row();
-	    table.add(buttonLvl5).size(ppuX * BUTTONLVL_WIDTH,ppuY * BUTTONLVL_HEIGHT).padBottom(20).padLeft(10).padRight(10);
-	    table.add(buttonLvl6).size(ppuX * BUTTONLVL_WIDTH,ppuY * BUTTONLVL_HEIGHT).padBottom(20).padLeft(10).padRight(10);
-	    table.add(buttonLvl7).size(ppuX * BUTTONLVL_WIDTH,ppuY * BUTTONLVL_HEIGHT).padBottom(20).padLeft(10).padRight(10);
-	    table.add(buttonLvl8).size(ppuX * BUTTONLVL_WIDTH,ppuY * BUTTONLVL_HEIGHT).padBottom(20).padLeft(10).padRight(10);
-	    table.row(); 
-	    table.add(buttonLvl9).size(ppuX * BUTTONLVL_WIDTH,ppuY * BUTTONLVL_HEIGHT).padBottom(20).padLeft(10).padRight(10);
-	    table.add(buttonLvl10).size(ppuX * BUTTONLVL_WIDTH,ppuY * BUTTONLVL_HEIGHT).padBottom(20).padLeft(10).padRight(10);
-	    table.add(buttonLvl11).size(ppuX * BUTTONLVL_WIDTH,ppuY * BUTTONLVL_HEIGHT).padBottom(20).padLeft(10).padRight(10);
-	    table.add(buttonLvl12).size(ppuX * BUTTONLVL_WIDTH,ppuY * BUTTONLVL_HEIGHT).padBottom(20).padLeft(10).padRight(10);
+	    for(int i = 0; i < buttons.length()/buttonsInRow; i++){
+	    	table.row();
+	    	for(int j = i * buttonsInRow; j < buttonsInRow * (i + 1); j++){
+		    	table.add(buttons[j]).size(ppuX * BUTTONLVL_WIDTH,ppuY * BUTTONLVL_HEIGHT).padBottom(20).padLeft(10).padRight(10);
+			}
+	    }
         table.setFillParent(true);
         stage.addActor(table);
 
